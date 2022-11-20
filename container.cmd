c00a9083df1eb082cdbf10daa763341cfcb93051
jchassaing <jeremie.chassaing@gmail.com>
2022-11-20 16:31:22 +0100

    Host Redirects

--- container.cmd
+++ container.cmd
@@ -1,3 +1,3 @@
-dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer src/proxyo
+dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer src/proxy
 docker tag proxy:1.0.0 thinkbeforecoding/proxy:latest
 docker build src/blog -t thinkbeforecoding/blog:latest
--- src/proxy/Program.fs
+++ src/proxy/Program.fs
@@ -4,11 +4,20 @@ open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Configuration
 open System.Threading.Tasks
+open Microsoft.AspNetCore.Http
+open System.Collections.Generic
+open Microsoft.AspNetCore.Http.Extensions
 
 [<CLIMutable>]
 type Redirect =
     { Path: string
       Target: string}
+
+[<CLIMutable>]
+type HostRedirect =
+    { Host: string
+      Target: string}
+
 
 [<EntryPoint>]
 let main args =
@@ -18,6 +27,19 @@ let main args =
         builder.Configuration.GetSection("Redirects").Get<Redirect[]>() 
         |> Option.ofObj
         |> Option.defaultValue [||]
+    
+    let hostRedirects = 
+        let mappings = 
+            builder.Configuration.GetSection("HostRedirects").Get<HostRedirect[]>() 
+            |> Option.ofObj
+            |> Option.defaultValue [||]
+
+        let d = Dictionary(StringComparer.OrdinalIgnoreCase)
+        for mapping in mappings do
+            d.Add(mapping.Host, mapping.Target)
+        d
+
+        
 
     builder.Services.AddReverseProxy()
         .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
@@ -32,9 +54,24 @@ let main args =
                 c.Response.Headers.Add("Location", redirect.Target)
                 c.Response.StatusCode <- 301 } :> Task) ) |> ignore
 
+    
+
     app.MapReverseProxy() |> ignore
+
+    if hostRedirects.Count > 0 then
+        app.Use(fun (context: HttpContext) (next: RequestDelegate) ->
+            task {
+                match hostRedirects.TryGetValue(context.Request.Host.Host) with
+                | false, _ -> do!  next.Invoke(context)
+                | true, target -> 
+                    let location = UriBuilder(context.Request.GetEncodedUrl(), Host = target).Uri
+                    context.Response.Headers.Add("Location", string location )
+                    context.Response.StatusCode <- 301
+            } :> Task) |> ignore
 
     app.Run()
 
     0 // Exit code
+
+open System
 
