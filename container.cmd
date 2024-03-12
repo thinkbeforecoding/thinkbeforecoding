3742fe96896ff999f414cb1905e17255263c0288
jchassaing <jeremie.chassaing@gmail.com>
2024-03-12 16:44:57 +0100

    Use AOT alpine container for proxy

--- container.cmd
+++ container.cmd
@@ -1,3 +1,5 @@
-dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer src/proxy
-docker tag proxy:1.0.0 thinkbeforecoding/proxy:latest
+@REM dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer src/proxy
+@REM docker tag proxy:1.0.0 thinkbeforecoding/proxy:latest
+docker run --rm -v D:\dev\thinkbeforecoding\:/app -w /app mcr.microsoft.com/dotnet/nightly/sdk:8.0-alpine3.19-aot sh -c "dotnet tool restore; dotnet publish -c Release ./src/proxy -o ./src/proxy/publish"
+docker build .\src\proxy\ -t thinkbeforecoding/proxy:latest
 docker build src/blog -t thinkbeforecoding/blog:latest
